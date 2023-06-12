08e9227f1d434ecd011900426dcfdb02d770dabf
Kirill Velikanov <velik.kir@gmail.com>
2023-06-12 20:14:52 +0300

    clean up

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 
 	"game_leaderboards/m/v2/api/endpoints"
-	"game_leaderboards/m/v2/app/models"
 	repository "game_leaderboards/m/v2/app/repositories"
 )
 
@@ -75,148 +72,6 @@ func main() {
 
 	// Запуск веб-сервера на порту 8080
 	log.Fatal(http.ListenAndServe(":8888", r))
-
-}
-
-func (uh *UserHandler) usersRouteHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-
-	case http.MethodGet:
-		uh.getUsers(w, r)
-	case http.MethodPost:
-		uh.createUser(w, r)
-	default:
-		// Обработка других методов
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-}
-func (uh *UserHandler) userRouteHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-
-	case http.MethodGet:
-		uh.getUser(w, r)
-	case http.MethodPut:
-		uh.editUser(w, r)
-	default:
-		// Обработка других методов
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func (uh *UserHandler) getUsers(w http.ResponseWriter, r *http.Request) {
-	users, err := uh.userRepository.GetAllUsers()
-	if err != nil {
-		log.Println("Failed to get users:", err)
-		http.Error(w, "Failed to get users", http.StatusInternalServerError)
-		return
-	}
-
-	// Кодируем срез users в формат JSON
-	jsonData, err := json.MarshalIndent(users, "", "  ")
-
-	if err != nil {
-		log.Println("Failed to format users:", err)
-		http.Error(w, "Failed to format users", http.StatusInternalServerError)
-		return
-	}
-
-	// Устанавливаем заголовок Content-Type на application/json
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
-
-}
-
-func (uh *UserHandler) getUser(w http.ResponseWriter, r *http.Request) {
-	// Получение параметра id из маршрута
-	vars := mux.Vars(r)
-	idRaw := vars["id"]
-
-	id, err := strconv.Atoi(idRaw)
-	if err != nil {
-		fmt.Println("Ошибка преобразования строки в int:", err)
-		return
-	}
-
-	user, err := uh.userRepository.GetUserByID(id)
-
-	// Кодируем срез users в формат JSON
-	jsonData, err := json.MarshalIndent(user, "", "	")
-	if err != nil {
-		log.Println("Failed to format user:", err)
-		http.Error(w, "Failed to format user", http.StatusInternalServerError)
-		return
-	}
-
-	// Устанавливаем заголовок Content-Type на application/json
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonData)
-
-}
-func (uh *UserHandler) createUser(w http.ResponseWriter, r *http.Request) {
-
-	// Считываем тело запроса
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-
-	// Декодируем JSON в структуру User
-	var user models.User
-	err = json.Unmarshal(body, &user)
-	if err != nil {
-		http.Error(w, "Failed to decode JSON", http.StatusBadRequest)
-		return
-	}
-	createdUser, err := uh.userRepository.CreateUser(user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	jsonData, err := json.MarshalIndent(createdUser, "", "	")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		http.Error(w, "Failed to encode user", http.StatusBadRequest)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
-	w.Write(jsonData)
-
-}
-func (uh *UserHandler) editUser(w http.ResponseWriter, r *http.Request) {
-
-	// Считываем тело запроса
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-
-	// Декодируем JSON в структуру User
-	var user models.User
-	err = json.Unmarshal(body, &user)
-	vars := mux.Vars(r)
-	idRaw := vars["id"]
-
-	id, err := strconv.Atoi(idRaw)
-	user.ID = id
-	if err != nil {
-		http.Error(w, "Failed to decode JSON", http.StatusBadRequest)
-		return
-	}
-	newUser, err := uh.userRepository.EditUser(user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		http.Error(w, "Failed to create user", http.StatusBadRequest)
-		return
-	}
-	jsonData, err := json.MarshalIndent(newUser, "", "	")
-
-	w.Write(jsonData)
-	w.WriteHeader(http.StatusOK)
 
 }
 
